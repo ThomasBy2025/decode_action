1b7c51c15f04659910dbe893bde6874b3d36a3fb
action <action@github.com>
2025-09-06 15:02:26 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,166 +1,160 @@
-//Fri Sep 05 2025 12:50:13 GMT+0000 (Coordinated Universal Time)
+//Sat Sep 06 2025 15:02:26 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
 "use strict";
-
 Object.defineProperty(exports, "__esModule", {
   "value": true
 });
-const _0x763ec9 = require("axios"),
-  _0x24bc63 = 20,
-  _0x19430d = new Date("2035-09-15T10:05:49Z");
-function _0x47e8fc() {
-  const _0x1af29c = new Date();
-  return _0x1af29c > _0x19430d;
+const _0x2e4953 = require("axios"),
+  _0x1375de = 20,
+  _0x3d2377 = new Date("2035-09-15T10:05:49Z");
+function _0x5ca802() {
+  const _0x2cfb48 = new Date();
+  return _0x2cfb48 > _0x3d2377;
 }
-function _0x3f73ef(_0x167e16) {
-  const _0x388879 = _0x167e16?.["qishui_label_info"]?.["only_vip_playable"] === true ? " 【VIP】" : "";
+function _0x389e2c(_0x34190e) {
   return {
-    "id": _0x167e16.item_id,
-    "title": _0x167e16.title + _0x388879,
-    "artist": _0x167e16.author_info.name,
-    "artwork": _0x167e16.cover_url
+    "id": _0x34190e.item_id,
+    "title": _0x34190e.title,
+    "artist": _0x34190e.author_info.name,
+    "artwork": _0x34190e.cover_url
   };
 }
-function _0x16a38d(_0x5d9c67) {
-  const _0xbcae89 = _0x5d9c67?.["track"]?.["label_info"]?.["only_vip_playable"] === true ? " 【VIP】" : "",
-    _0x2853ad = _0x5d9c67.track.album.url_cover.uri,
-    _0xc3e6f1 = _0x5d9c67.track.album.url_cover.template_prefix;
+function _0x4d008d(_0x35a3c7) {
+  const _0x556522 = _0x35a3c7.track.album.url_cover.uri,
+    _0xa3a9c3 = _0x35a3c7.track.album.url_cover.template_prefix;
   return {
-    "id": _0x5d9c67.track.id,
-    "title": _0x5d9c67.track.name + _0xbcae89,
-    "artist": _0x5d9c67.track.artists[0].name,
-    "artistId": _0x5d9c67.track.artists[0].id,
-    "album": _0x5d9c67.track.album.name,
-    "albumId": _0x5d9c67.track.album.id,
-    "artwork": "https://p3-luna.douyinpic.com/img/" + _0x2853ad + "~" + _0xc3e6f1 + "-resize:960:960.png"
+    "id": _0x35a3c7.track.id,
+    "title": _0x35a3c7.track.name,
+    "artist": _0x35a3c7.track.artists[0].name,
+    "artistId": _0x35a3c7.track.artists[0].id,
+    "album": _0x35a3c7.track.album.name,
+    "albumId": _0x35a3c7.track.album.id,
+    "artwork": "https://p3-luna.douyinpic.com/img/" + _0x556522 + "~" + _0xa3a9c3 + "-resize:960:960.png"
   };
 }
-function _0x49565e(_0x5b7816) {
-  const _0xfe24c4 = _0x5b7816?.["label_info"]?.["only_vip_playable"] === true ? " 【VIP】" : "",
-    _0x1f70b4 = _0x5b7816.album.url_cover.uri,
-    _0x19277a = _0x5b7816.album.url_cover.template_prefix;
+function _0x344242(_0x5959c9) {
+  const _0x1bbb17 = _0x5959c9.album.url_cover.uri,
+    _0x5ef8d0 = _0x5959c9.album.url_cover.template_prefix;
   return {
-    "id": _0x5b7816.id,
-    "title": _0x5b7816.name + _0xfe24c4,
-    "artist": _0x5b7816.artists[0].name,
-    "artistId": _0x5b7816.artists[0].id,
-    "album": _0x5b7816.album.name,
-    "albumId": _0x5b7816.album.id,
-    "artwork": "https://p3-luna.douyinpic.com/img/" + _0x1f70b4 + "~" + _0x19277a + "-resize:960:960.png"
+    "id": _0x5959c9.id,
+    "title": _0x5959c9.name,
+    "artist": _0x5959c9.artists[0].name,
+    "artistId": _0x5959c9.artists[0].id,
+    "album": _0x5959c9.album.name,
+    "albumId": _0x5959c9.album.id,
+    "artwork": "https://p3-luna.douyinpic.com/img/" + _0x1bbb17 + "~" + _0x5ef8d0 + "-resize:960:960.png"
   };
 }
-function _0x16803b(_0x1f38af) {
-  const _0x57c539 = _0x1f38af?.["entity"]?.["track_wrapper"]?.["track"]?.["label_info"]?.["only_vip_playable"] === true ? " 【VIP】" : "",
-    _0x4f925b = _0x1f38af.entity.track_wrapper.track.album.url_cover.uri,
-    _0x412208 = _0x1f38af.entity.track_wrapper.track.album.url_cover.template_prefix;
+function _0x4c03f6(_0x517e58) {
+  const _0xc813ff = _0x517e58.entity.track_wrapper.track.album.url_cover.uri,
+    _0x4f2668 = _0x517e58.entity.track_wrapper.track.album.url_cover.template_prefix;
   return {
-    "id": _0x1f38af.entity.track_wrapper.track.id,
-    "title": _0x1f38af.entity.track_wrapper.track.name + _0x57c539,
-    "artist": _0x1f38af.entity.track_wrapper.track.artists[0].name,
-    "artistId": _0x1f38af.entity.track_wrapper.track.artists[0].id,
-    "album": _0x1f38af.entity.track_wrapper.track.album.name,
-    "albumId": _0x1f38af.entity.track_wrapper.track.album.id,
-    "artwork": "https://p3-luna.douyinpic.com/img/" + _0x4f925b + "~" + _0x412208 + "-resize:960:960.png"
+    "id": _0x517e58.entity.track_wrapper.track.id,
+    "title": _0x517e58.entity.track_wrapper.track.name,
+    "artist": _0x517e58.entity.track_wrapper.track.artists[0].name,
+    "artistId": _0x517e58.entity.track_wrapper.track.artists[0].id,
+    "album": _0x517e58.entity.track_wrapper.track.album.name,
+    "albumId": _0x517e58.entity.track_wrapper.track.album.id,
+    "artwork": "https://p3-luna.douyinpic.com/img/" + _0xc813ff + "~" + _0x4f2668 + "-resize:960:960.png"
   };
 }
-function _0x28e674(_0x3784fa) {
-  const _0x21a73c = _0x3784fa?.["resources"][0]?.["entity"]?.["playlist"]?.["label_info"]?.["only_vip_playable"] === true ? " 【VIP】" : "",
-    _0x17ddc0 = _0x3784fa.resources[0].entity.playlist.url_cover.uri,
-    _0x24755f = _0x3784fa.resources[0].entity.playlist.url_cover.template_prefix;
+function _0x5b910a(_0x34a9eb) {
+  const _0x37c4c2 = _0x34a9eb.resources[0].entity.playlist.url_cover.uri,
+    _0x6309d5 = _0x34a9eb.resources[0].entity.playlist.url_cover.template_prefix;
   return {
-    "id": _0x3784fa.resources[0].entity.playlist.id,
-    "title": _0x3784fa.resources[0].entity.playlist.title + _0x21a73c,
-    "artist": _0x3784fa.resources[0].entity.playlist.owner.nickname,
-    "createUserId": _0x3784fa.resources[0].entity.playlist.owner.id,
-    "description": _0x3784fa.resources[0].entity.playlist.desc,
-    "artwork": "https://p3-luna.douyinpic.com/img/" + _0x17ddc0 + "~" + _0x24755f + "-resize:960:960.png",
-    "createTime": _0x3784fa.resources[0].entity.playlist.create_time
+    "id": _0x34a9eb.resources[0].entity.playlist.id,
+    "title": _0x34a9eb.resources[0].entity.playlist.title,
+    "artist": _0x34a9eb.resources[0].entity.playlist.owner.nickname,
+    "createUserId": _0x34a9eb.resources[0].entity.playlist.owner.id,
+    "description": _0x34a9eb.resources[0].entity.playlist.desc,
+    "artwork": "https://p3-luna.douyinpic.com/img/" + _0x37c4c2 + "~" + _0x6309d5 + "-resize:960:960.png",
+    "createTime": _0x34a9eb.resources[0].entity.playlist.create_time
   };
 }
-function _0x5f5bca(_0x499aea) {
-  const _0x485fc5 = new Date(_0x499aea.time_created);
+function _0x46ac93(_0x494a53) {
+  const _0x3b7c84 = new Date(_0x494a53.time_created);
   return {
-    "id": _0x499aea.id,
-    "nickName": _0x499aea.user.nickname,
-    "avatar": _0x499aea.user.medium_avatar_url && _0x499aea.user.medium_avatar_url.urls[0],
-    "comment": _0x499aea.content,
-    "like": _0x499aea.count_digged,
-    "createAt": _0x485fc5.toLocaleString()
+    "id": _0x494a53.id,
+    "nickName": _0x494a53.user.nickname,
+    "avatar": _0x494a53.user.medium_avatar_url && _0x494a53.user.medium_avatar_url.urls[0],
+    "comment": _0x494a53.content,
+    "like": _0x494a53.count_digged,
+    "createAt": _0x3b7c84.toLocaleString()
   };
 }
-const _0x457aa7 = {
+const _0x349d33 = {
   "Accept": "*/*",
   "Content-Type": "application/json; charset=UTF-8",
   "User-Agent": "com.luna.music/100159040 (Linux; U; Android 11; zh_CN; Cronet/TTNetVersion:dd1b0931 2024-06-28 QuicVersion:d299248d 2024-04-09)",
   "X-Argus": "=",
   "x-common-params-v2": "channel=appstore&aid=8478&device_id=1100210274091033"
 };
-async function _0x58e6db(_0x2884b3, _0x38f01c) {
-  if (_0x47e8fc()) {
-    return;
-  }
-  const _0x16bb25 = (_0x38f01c - 1) * _0x24bc63,
-    _0xb1c311 = (await _0x763ec9.default.get("https://api-vehicle.volcengine.com/v2/search/type", {
+async function _0x258c72(_0x362c11, _0x579069) {
+  if (_0x5ca802()) return;
+  const _0x4fc438 = (_0x579069 - 1) * _0x1375de,
+    _0x528501 = (await _0x2e4953.default.get("https://api-vehicle.volcengine.com/v2/search/type", {
       "params": {
-        "keyword": _0x2884b3,
+        "keyword": _0x362c11,
         "search_type": "music",
-        "limit": _0x24bc63,
-        "real_offset": _0x16bb25,
+        "limit": _0x1375de,
+        "real_offset": _0x4fc438,
         "search_source": "qishui"
       }
     })).data,
-    _0x573f97 = _0xb1c311.data.list.map(_0x3f73ef);
+    _0x176d80 = _0x528501.data.list.map(_0x389e2c);
   return {
-    "isEnd": _0xb1c311.data.list.length === 0 || _0xb1c311.data.list.length < _0x24bc63 ? true : false,
-    "data": _0x573f97
+    "isEnd": _0x528501.data.list.length === 0 || _0x528501.data.list.length < _0x1375de ? true : false,
+    "data": _0x176d80
   };
 }
-async function _0x86473c(_0x43dca9) {
-  if (_0x47e8fc()) return;
-  const _0x2acb4a = (await _0x763ec9.default.get("https://api-vehicle.volcengine.com/v2/custom/contents", {
+async function _0x11caff(_0x887abd) {
+  if (_0x5ca802()) return;
+  const _0x48be53 = (await _0x2e4953.default.get("https://api-vehicle.volcengine.com/v2/custom/contents", {
     "params": {
       "sources": "qishui",
       "need_author": true,
       "need_album": true,
       "need_ugc": true,
       "need_stat": true,
-      "item_ids": _0x43dca9.id
+      "item_ids": _0x887abd.id
     }
   })).data;
   return {
-    "artwork": _0x2acb4a.data.list[0].cover_url,
-    "rawLrc": _0x2acb4a.data.list[0].lyric_info.lyric_text
+    "artwork": _0x48be53.data.list[0].cover_url,
+    "rawLrc": _0x48be53.data.list[0].lyric_info.lyric_text
   };
 }
-async function _0x49e305(_0x1214a0, _0x3e5938) {
-  const _0x49f1a6 = "https://beta-luna.douyin.com/luna/h5/seo_track?track_id=" + _0x1214a0.id + "&device_platform=web",
-    _0x2d6a1c = (await _0x763ec9.default.get(_0x49f1a6)).data;
+async function _0x1085bb(_0x47cb43, _0xecdc3d) {
+  const _0x4b4f89 = "https://beta-luna.douyin.com/luna/h5/seo_track?track_id=" + _0x47cb43.id + "&device_platform=web",
+    _0xc95d7 = (await _0x2e4953.default.get(_0x4b4f89)).data;
   try {
-    const _0x1e69ff = (await _0x763ec9.default.get(_0x2d6a1c.track_player.url_player_info)).data,
-      _0x22ec68 = _0x1e69ff.Result.Data.PlayInfoList,
-      _0x247e36 = _0x22ec68[_0x22ec68.length - 1],
-      _0x58af55 = _0x247e36.MainPlayUrl;
-    return {
-      "url": _0x58af55.replace("audio_mp4", "audio_mp3"),
-      "headers": {
-        "Accept": "*/*",
-        "Accept-Language": "zh-CN,zh;q=0.9",
-        "Cache-Control": "no-cache",
-        "Pragma": "no-cache",
-        "Range": "bytes=0-",
-        "Referer": "https://www.douyin.com/",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
-      }
-    };
-  } catch (_0x353baf) {
-    console.warn("播放异常", _0x353baf);
+    {
+      const _0x33c306 = (await _0x2e4953.default.get(_0xc95d7.track_player.url_player_info)).data,
+        _0x2a9c69 = _0x33c306.Result.Data.PlayInfoList,
+        _0xc12215 = _0x2a9c69[_0x2a9c69.length - 1],
+        _0x145d4b = _0xc12215.MainPlayUrl;
+      return {
+        "url": _0x145d4b.replace("audio_mp4", "audio_mp3"),
+        "headers": {
+          "Accept": "*/*",
+          "Accept-Language": "zh-CN,zh;q=0.9",
+          "Cache-Control": "no-cache",
+          "Pragma": "no-cache",
+          "Range": "bytes=0-",
+          "Referer": "https://www.douyin.com/",
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+        }
+      };
+    }
+  } catch (_0x27e15d) {
+    console.warn("播放异常", _0x27e15d);
     return {
       "url": ""
     };
   }
 }
-async function _0x5d79f5() {
+async function _0xc706e9() {
   return [{
     "title": "默认排行榜",
     "data": [{
@@ -186,57 +180,63 @@ async function _0x5d79f5() {
     }]
   }];
 }
-async function _0x71761d(_0xac89b1, _0x3b2135 = 1) {
-  if (_0x47e8fc()) return;
-  const _0x2dc1b6 = (await _0x763ec9.default.get("https://api5-lf.qishui.com/luna/charts/" + _0xac89b1.id + "?charge=0", {
-    "headers": _0x457aa7
+async function _0x1089fe(_0x23c62e, _0x277b6a = 1) {
+  if (_0x5ca802()) {
+    return;
+  }
+  const _0x48d09b = (await _0x2e4953.default.get("https://api5-lf.qishui.com/luna/charts/" + _0x23c62e.id + "?charge=0", {
+    "headers": _0x349d33
   })).data;
-  return Object.assign(Object.assign({}, _0xac89b1), {
-    "musicList": _0x2dc1b6.chart.track_ranks.map(_0x16a38d)
+  return Object.assign(Object.assign({}, _0x23c62e), {
+    "musicList": _0x48d09b.chart.track_ranks.map(_0x4d008d)
   });
 }
-async function _0x44bcb3(_0x54ca83) {
-  if (_0x47e8fc()) return;
-  let _0x261e17;
-  !_0x261e17 && (_0x261e17 = (_0x54ca83.match(/https?:\/\/(.*?).douyin.com\/qishui\/share\/playlist\?playlist_id=([0-9]+)/) || [])[2]);
-  if (!_0x261e17) {
-    _0x261e17 = (_0x54ca83.match(/^(\d+)$/) || [])[1];
+async function _0x339811(_0x14569f) {
+  if (_0x5ca802()) {
+    return;
   }
-  if (!_0x261e17) return;
-  const _0x2059d4 = _0x8ea7c1(_0x261e17);
-  return _0x2059d4.data.media_resources.map(_0x16803b);
+  let _0x463171;
+  !_0x463171 && (_0x463171 = (_0x14569f.match(/https?:\/\/(.*?).douyin.com\/qishui\/share\/playlist\?playlist_id=([0-9]+)/) || [])[2]);
+  !_0x463171 && (_0x463171 = (_0x14569f.match(/^(\d+)$/) || [])[1]);
+  if (!_0x463171) {
+    return;
+  }
+  const _0x327f8a = _0xc56b05(_0x463171);
+  return _0x327f8a.data.media_resources.map(_0x4c03f6);
 }
-async function _0x8ea7c1(_0x3cfb88) {
-  if (_0x47e8fc()) return;
+async function _0xc56b05(_0x3a3be6) {
+  if (_0x5ca802()) {
+    return;
+  }
   try {
-    return await _0x763ec9.default.post("https://api5-lq.qishui.com/luna/playlist/detail?charge=0", {
-      "playlist_id": _0x3cfb88
+    return await _0x2e4953.default.post("https://api5-lq.qishui.com/luna/playlist/detail?charge=0", {
+      "playlist_id": _0x3a3be6
     }, {
-      "headers": _0x457aa7
+      "headers": _0x349d33
     });
-  } catch (_0x1a9fdf) {
+  } catch (_0x1d8255) {
     return [];
   }
 }
-async function _0x179df7(_0x84f314, _0xa0c186, _0x386d54) {
-  if (_0x47e8fc()) return;
-  const _0x7f56c5 = (await _0x763ec9.default.get("https://api5-lq.qishui.com/luna/artists/" + _0x84f314.id + "/tracks?count=1000&charge=0", {
-    "headers": _0x457aa7
+async function _0x1c5913(_0x12af0c, _0x1310d3, _0x5a94a5) {
+  if (_0x5ca802()) return;
+  const _0x540a83 = (await _0x2e4953.default.get("https://api5-lq.qishui.com/luna/artists/" + _0x12af0c.id + "/tracks?count=1000&charge=0", {
+    "headers": _0x349d33
   })).data;
   return {
-    "data": _0x7f56c5.tracks.map(_0x49565e)
+    "data": _0x540a83.tracks.map(_0x344242)
   };
 }
-async function _0x1d5a12(_0x21226a) {
-  if (_0x47e8fc()) return;
-  const _0x49786f = (await _0x763ec9.default.get("https://api5-lq.qishui.com/luna/albums/" + _0x21226a.id + "?count=1000&charge=0", {
-    "headers": _0x457aa7
+async function _0x79f5d7(_0x4d0056) {
+  if (_0x5ca802()) return;
+  const _0xcac173 = (await _0x2e4953.default.get("https://api5-lq.qishui.com/luna/albums/" + _0x4d0056.id + "?count=1000&charge=0", {
+    "headers": _0x349d33
   })).data;
   return {
-    "musicList": _0x49786f.tracks.map(_0x49565e)
+    "musicList": _0xcac173.tracks.map(_0x344242)
   };
 }
-async function _0x5e1360() {
+async function _0x18623a() {
   return {
     "data": [],
     "pinned": [{
@@ -281,51 +281,55 @@ async function _0x5e1360() {
     }]
   };
 }
-async function _0x4f9d9d(_0x556689, _0xe596e7) {
-  if (_0x47e8fc()) return;
-  let _0x20dbc5 = Number.isNaN(parseInt(_0x556689.id, 10)) ? 0 : parseInt(_0x556689.id, 10);
+async function _0x4e0fa1(_0xbaa80a, _0x1fbb22) {
+  if (_0x5ca802()) {
+    return;
+  }
+  let _0x2e5d25 = Number.isNaN(parseInt(_0xbaa80a.id, 10)) ? 0 : parseInt(_0xbaa80a.id, 10);
   try {
-    const _0x16b8db = await _0x763ec9.default.post("https://api5-lq.qishui.com/luna/discover/mix?charge=0", {
+    const _0x241856 = await _0x2e4953.default.post("https://api5-lq.qishui.com/luna/discover/mix?charge=0", {
       "block_type": "discover_playlist_mix",
       "feed_discover_extra": {},
       "latest_douyin_liked_playlist_show_ts": 0,
-      "sub_channel_id": _0x20dbc5
+      "sub_channel_id": _0x2e5d25
     }, {
-      "headers": _0x457aa7
+      "headers": _0x349d33
     });
     return {
       "isEnd": false,
-      "data": _0x16b8db.data.inner_block.map(_0x28e674)
+      "data": _0x241856.data.inner_block.map(_0x5b910a)
     };
-  } catch (_0x45ea05) {
+  } catch (_0x7f1c66) {
     return {
       "isEnd": false,
       "data": []
     };
   }
 }
-async function _0xe4a37d(_0x539e85, _0x6ae96b = 1) {
-  if (_0x47e8fc()) return;
-  const _0x5784e4 = (_0x6ae96b - 1) * _0x24bc63,
-    _0x327060 = (await _0x763ec9.default.get("https://api5-lq.qishui.com/luna/comments?group_id=" + _0x539e85.id + "&cursor=" + _0x5784e4 + "&count=" + _0x24bc63 + "&charge=0", {
-      "headers": _0x457aa7
+async function _0x1aaf09(_0x21aa12, _0x1ef6fc = 1) {
+  if (_0x5ca802()) {
+    return;
+  }
+  const _0x2d3115 = (_0x1ef6fc - 1) * _0x1375de,
+    _0x31a114 = (await _0x2e4953.default.get("https://api5-lq.qishui.com/luna/comments?group_id=" + _0x21aa12.id + "&cursor=" + _0x2d3115 + "&count=" + _0x1375de + "&charge=0", {
+      "headers": _0x349d33
     })).data;
   return {
-    "isEnd": _0x6ae96b * _0x24bc63 > _0x327060.count ? true : false,
-    "data": _0x327060.comments.map(_0x5f5bca)
+    "isEnd": _0x1ef6fc * _0x1375de > _0x31a114.count ? true : false,
+    "data": _0x31a114.comments.map(_0x46ac93)
   };
 }
-async function _0x1b66fc(_0x1876ba) {
-  if (_0x47e8fc()) return;
-  const _0x577803 = await _0x8ea7c1(_0x1876ba.id);
+async function _0x2068be(_0x100c06) {
+  if (_0x5ca802()) return;
+  const _0x302a54 = await _0xc56b05(_0x100c06.id);
   return {
     "isEnd": true,
-    "musicList": _0x577803.data.media_resources.map(_0x16803b)
+    "musicList": _0x302a54.data.media_resources.map(_0x4c03f6)
   };
 }
 module.exports = {
   "platform": "开心汽水",
-  "version": "0.1.5",
+  "version": "0.1.3",
   "author": "鸿蒙",
   "appVersion": ">0.1.0-alpha.0",
   "srcUrl": "https://gitee.com/hongmengv5/musicfree/raw/master/qishui.js",
@@ -334,19 +338,21 @@ module.exports = {
     "importMusicSheet": ["汽水APP：歌单-分享-分享链接；手动访问链接后再复制链接粘贴即可", "网页：复制URL并粘贴，或者直接输入纯数字歌单ID即可", "导入时间和歌单大小有关，请耐心等待"]
   },
   "supportedSearchType": ["music"],
-  async "search"(_0x1f0edd, _0x56a1c0, _0x3dbf36) {
-    if (_0x3dbf36 === "music") return await _0x58e6db(_0x1f0edd, _0x56a1c0);
+  async "search"(_0x42bc0c, _0xa962a9, _0x21e409) {
+    if (_0x21e409 === "music") {
+      return await _0x258c72(_0x42bc0c, _0xa962a9);
+    }
   },
-  "getMusicInfo": _0x86473c,
-  "getLyric": _0x86473c,
-  "getMediaSource": _0x49e305,
-  "getTopLists": _0x5d79f5,
-  "getTopListDetail": _0x71761d,
-  "importMusicSheet": _0x44bcb3,
-  "getArtistWorks": _0x179df7,
-  "getAlbumInfo": _0x1d5a12,
-  "getRecommendSheetTags": _0x5e1360,
-  "getRecommendSheetsByTag": _0x4f9d9d,
-  "getMusicSheetInfo": _0x1b66fc,
-  "getMusicComments": _0xe4a37d
+  "getMusicInfo": _0x11caff,
+  "getLyric": _0x11caff,
+  "getMediaSource": _0x1085bb,
+  "getTopLists": _0xc706e9,
+  "getTopListDetail": _0x1089fe,
+  "importMusicSheet": _0x339811,
+  "getArtistWorks": _0x1c5913,
+  "getAlbumInfo": _0x79f5d7,
+  "getRecommendSheetTags": _0x18623a,
+  "getRecommendSheetsByTag": _0x4e0fa1,
+  "getMusicSheetInfo": _0x2068be,
+  "getMusicComments": _0x1aaf09
 };
